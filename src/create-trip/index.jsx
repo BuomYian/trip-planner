319488d1ad08ea7b0d8c8e81904fbb1921eb1f96
Buomkuoth Yian <buom.nyajal@gmail.com>
2024-12-25 21:36:10 +0300

    Refactored firbase realtime database

--- src/create-trip/index.jsx
+++ src/create-trip/index.jsx
@@ -9,11 +9,26 @@ import {
 import { chatSession } from "@/service/AiModal";
 import { useState, useEffect } from "react";
 import GooglePlacesAutocomplete from "react-google-places-autocomplete";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
+import { FcGoogle } from "react-icons/fc";
+import { AiOutlineLoading3Quarters } from "react-icons/ai";
+import { useGoogleLogin } from "@react-oauth/google";
+import axios from "axios";
+import { ref, set } from "firebase/database";
+import { db } from "@/service/FirebaseConfig";
 
 const CreateTrip = () => {
   const [place, setPlace] = useState();
   const { toast } = useToast();
   const [formData, setFormData] = useState([]);
+  const [openDialog, setOpenDialog] = useState(false);
+  const [loading, setLoading] = useState(false);
 
   const handleInputChange = (name, value) => {
     setFormData({ ...formData, [name]: value });
@@ -23,7 +38,21 @@ const CreateTrip = () => {
     console.log(formData);
   }, [formData]);
 
+  const login = useGoogleLogin({
+    onSuccess: (userRes) => getUserProfile(userRes),
+    onError: (error) => {
+      console.log(error);
+    },
+  });
+
   const onGeneratedTrip = async () => {
+    const user = localStorage.getItem("user");
+
+    if (!user) {
+      setOpenDialog(true);
+      return;
+    }
+
     if (
       !formData.noOfDays ||
       !formData.location ||
@@ -38,6 +67,7 @@ const CreateTrip = () => {
       return;
     }
 
+    setLoading(true);
     const FINAL_PROMPT = AI_PROMPT.replace(
       `{location}`,
       formData.location.label
@@ -47,9 +77,60 @@ const CreateTrip = () => {
       .replace(`{budget}`, formData.budget)
       .replace(`{noOfdays}`, formData.noOfDays);
 
-    console.log(FINAL_PROMPT);
+    // console.log(FINAL_PROMPT);
     const result = await chatSession.sendMessage(FINAL_PROMPT);
     console.log(result.response.text());
+
+    saveAiTrip(result.response.text());
+    setLoading(false);
+  };
+
+  const saveAiTrip = async (TripData) => {
+    try {
+      setLoading(true);
+      const userInfo = JSON.parse(localStorage.getItem("user"));
+      const docId = Date.now().toString();
+
+      await set(ref(db, 'AITrips/' + docId), {
+        userSelection: formData,
+        tripData: JSON.parse(TripData),
+        userEmail: userInfo?.email,
+        id: docId,
+      });
+
+      toast({
+        title: "Success",
+        description: "Trip saved successfully!",
+      });
+      setLoading(false);
+    } catch (error) {
+      console.error("Error saving trip:", error);
+      toast({
+        variant: "destructive",
+        title: "Error",
+        description: "Failed to save trip. Please try again.",
+      });
+      setLoading(false);
+    }
+  };
+
+  const getUserProfile = (tokenInfo) => {
+    axios
+      .get(
+        `https://www.googleapis.com/oauth2/v1/userinfo?alt=json&access_token=${tokenInfo?.access_token}`,
+        {
+          headers: {
+            Authorization: `Bearer ${tokenInfo?.access_token}`,
+            Accept: "Application/json",
+          },
+        }
+      )
+      .then((res) => {
+        console.log(res);
+        localStorage.setItem("user", JSON.stringify(res.data));
+        setOpenDialog(false);
+        onGeneratedTrip();
+      });
   };
 
   return (
@@ -127,13 +208,39 @@ const CreateTrip = () => {
       </div>
       <div className="flex justify-end">
         <Button
+          disabled={loading}
           type="submit"
           onClick={onGeneratedTrip}
           className="bg-[#f56551] text-white font-bold py-2 px-5 rounded-lg mt-10"
         >
-          Generate Trip
+          {loading ? (
+            <AiOutlineLoading3Quarters className="h-7 w-7 animate-spin" />
+          ) : (
+            "Generate Trip"
+          )}
         </Button>
       </div>
+
+      <Dialog open={openDialog}>
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle className="font-bold text-lg mt-7">
+              Sign in with Google
+            </DialogTitle>
+            <DialogDescription>
+              To generate a trip, you need to sign in with your google account
+              <Button
+                disabled={loading}
+                onClick={login}
+                className="bg-[#f56551] text-white font-bold py-2 px-5 rounded-lg mt-10"
+              >
+                <FcGoogle className="text-2xl" />
+                Sign in With Google
+              </Button>
+            </DialogDescription>
+          </DialogHeader>
+        </DialogContent>
+      </Dialog>
     </div>
   );
 };
---
+++ src/service/FirebaseConfig.jsx
@@ -0,0 +1,23 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getDatabase } from "firebase/database";
+// import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCez32JjSoA5FS4MnLYbGMtf7hCOw9IZ5Y",
+  authDomain: "ai-trip-planner-18a6c.firebaseapp.com",
+  projectId: "ai-trip-planner-18a6c",
+  storageBucket: "ai-trip-planner-18a6c.firebasestorage.app",
+  messagingSenderId: "643230880769",
+  appId: "1:643230880769:web:74ec8bc4ae88108718dc00",
+  measurementId: "G-Z4BB8GM7S6",
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+export const db = getDatabase(app);
+// const analytics = getAnalytics(app);
